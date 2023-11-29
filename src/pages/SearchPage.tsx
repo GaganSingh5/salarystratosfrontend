c7c99f376fea0f81513c2f692852359faca7e821
GaganSingh5 <gagansingh0797@gmail.com>
2023-11-29 15:20:12 -0500

    code changes

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,23 +1,96 @@
-import { useState } from "react";
-import {Container, Form, Row, Col, Button, Card, Badge, Stack, Accordion } from "react-bootstrap";
+import { useCallback, useEffect, useState, useMemo } from "react";
+import {
+  Container,
+  Form,
+  Row,
+  Col,
+  Button,
+  Card,
+  Badge,
+  Stack,
+  Accordion,
+  ListGroup,
+} from "react-bootstrap";
 
 function SearchPage() {
-
   const [searchInput, setSearchInput] = useState("");
-  const [jobList, setJobList] = useState([1,2,3,4, 5, 6]);
+  const [jobList, setJobList] = useState([1, 2, 3, 4, 5, 6]);
   const [suggestedWords, setSuggestedWords] = useState([]);
+  const [recentSearch, setRecentSearch] = useState({});
+  const [correctWords, setCorrectWords] = useState([]);
 
+  const onSearch = () => {
+    fetch("http://localhost:8080/api/correctWords", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/x-www-form-urlencoded", // <-- Specifying the Content-Type
+      }),
+      body: `searchTerm=${searchInput}&suggestionCount=${5}`,
+    }).then(async (data) => {
+      const response = await data.json();
+      console.log(response);
+      setCorrectWords(response);
+    });
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/recentSearches", {
+      method: "Get",
+    }).then(async (data) => {
+      const response = await data.json();
+      console.log(typeof response);
+      console.log(Object.keys(response));
+      setRecentSearch(response);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log("executed");
+    const timeoutFunction = setTimeout(() => {
+      fetch("http://localhost:8080/api/wordSuggestions", {
+        method: "POST",
+        headers: new Headers({
+          "Content-Type": "application/x-www-form-urlencoded", // <-- Specifying the Content-Type
+        }),
+        body: `searchTerm=${searchInput}&suggestionCount=${5}`,
+      }).then(async (data) => {
+        const response = await data.json();
+        console.log(response);
+
+        let object = {};
+        const arr = [];
+        // if ()
+        setSuggestedWords(
+          response.wordSuggestions.map((data) => {
+            object = {
+              word: data.word,
+              suggestions: data.suggestedWords,
+            };
+            arr.push(object);
+          })
+        );
+
+        console.log(arr);
+
+        setSuggestedWords(arr);
+      });
+    }, 1000);
+
+    return () => clearTimeout(timeoutFunction);
+  }, [searchInput]);
 
   const searchForJobs = () => {
-    fetch("http://localhost:8080/wordSuggestions").then(async (data) => {
+    fetch("http://localhost:8080/api/wordSuggestions", {
+      method: "POST",
+      body: searchInput,
+    }).then(async (data) => {
       const result = await data.json();
       console.log(result);
     });
-  }
-  
+  };
 
   return (
-    <Container className="vw-100 vh-100 p-4 justify-center">
+    <Container className="vw-100 p-4 justify-center">
       <Row className="vw-90 mb-3" xs="auto">
         <Col lg={5} className="vw-60 px-0">
           <Form>
@@ -33,7 +106,7 @@ function SearchPage() {
           </Form>
         </Col>
         <Col className="pl-2 align-middle d-flex">
-          <Button onClick={() => searchForJobs()} variant="primary">
+          <Button onClick={() => onSearch()} variant="primary">
             Search
           </Button>
         </Col>
@@ -57,15 +130,129 @@ function SearchPage() {
           </Button>
         </Col>
       </Row>
-      <Row>
+      <Row className="mb-2">
+        <Col lg={5} className="p-0">
+          <Card>
+            <Card.Body>
+              <Card.Title>Recent Searches With Frequency Count</Card.Title>
+              <Card.Text>
+                <Stack
+                  style={{ flexWrap: "wrap" }}
+                  direction="horizontal"
+                  gap={2}
+                >
+                  {recentSearch &&
+                    Object.keys(recentSearch) &&
+                    Object.keys(recentSearch)?.map((word) => {
+                      return (
+                        <Badge className="badge rounded-pill" bg="primary">
+                          {word}: {recentSearch[word]}
+                        </Badge>
+                      );
+                    })}
+                </Stack>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mb-2">
         <Card>
           <Card.Body>
-            <Card.Title>Suggestions</Card.Title>
-            <Card.Text>
+            <Card.Title>Word Suggestions</Card.Title>
+
+            {suggestedWords.map((data) => {
+              const sugg = data.suggestions.dataList;
+              console.log(sugg);
+
+              const badges = sugg.map((wordData) => {
+                return (
+                  <ListGroup>
+                    <ListGroup.Item>Frequency: {wordData["frequency"]}</ListGroup.Item>
+                    <ListGroup.Item>
+                      <Badge className="badge rounded-pill" bg="primary">
+                              {wordData["word"]}
+                            </Badge>
+                    </ListGroup.Item>
+                  </ListGroup>
+                );
+              });
+              return (
+                <Card.Text>
+                  <Stack
+                    style={{ flexWrap: "wrap" }}
+                    direction="horizontal"
+                    gap={2}
+                  >
+                    <h5>{data?.word}</h5>: {badges}
+                  </Stack>
+                  
+                </Card.Text>
+              );
+            })}
+            {/* <Card.Text>
               <Stack direction="horizontal" gap={2}>
                 <Badge bg="primary">Primary</Badge>
                 <Badge bg="secondary">Secondary</Badge>
               </Stack>
+            </Card.Text> */}
+          </Card.Body>
+        </Card>
+      </Row>
+      <Row className="mb-2">
+        <Card>
+          <Card.Body>
+            <Card.Title>Spell Checker</Card.Title>
+            <Card.Text>
+                {correctWords.map((word) => {
+                  if (word.correctWords !== null) {
+                    const result = Object.keys(word.correctWords).map((key)=>{
+                      const data = word.correctWords[key];
+                      const countKeys = Object.keys(data).map((d)=>Number(d));
+                      const maxCount = Math.max(...countKeys);
+                      const wordArray = data[maxCount];
+
+                      return (
+                        <ListGroup>
+                          <ListGroup.Item>EditCost: {key}</ListGroup.Item>
+                          <ListGroup.Item>Frequency: {maxCount}</ListGroup.Item>
+                          <ListGroup.Item>
+                            <Stack direction="horizontal" gap={2}>
+                              {wordArray.map((word) => {
+                                return (
+                                  <Badge
+                                    className="badge rounded-pill"
+                                    bg="primary"
+                                  >
+                                    {word}
+                                  </Badge>
+                                );
+                              })}
+                            </Stack>
+                          </ListGroup.Item>
+                        </ListGroup>
+                      );
+
+                    })
+                    console.log(result);
+                    
+                    return (
+                      <>
+                        <h5 className="d-flex">{word.word}</h5>
+                        <Stack direction="horizontal" gap={2}>
+                          {result}
+                        </Stack>
+                      </>
+                    );
+                  } else {
+                    return (
+                      <div>
+                        <h5>{word.word}</h5>
+                        <p>Search terms are correct</p>
+                      </div>
+                    );
+                  }
+                })}
             </Card.Text>
           </Card.Body>
         </Card>
